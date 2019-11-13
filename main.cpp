61f285ee363c5a04e3e0dc95894cd28dec87a800
abdoul gbadamassi <gbadhanane@gmail.com>
2019-11-13 03:29:38 +0000

    commit

--- main.cpp
+++ main.cpp
@@ -5,7 +5,12 @@ using namespace std;
 // virtual polimorphism
 
 int main() {
- // Ugrad k1;
+    Ugrad k1(3.5, "CS", "123", "Paul");
+   
+    k1.print();
+
+    //another method to print 
+    cout<<"Here is another method to print"<< end;
     Ugrad *k1= new Ugrad(3.5, "CS", "123", "Paul");
-    k1->print();
+    k1->print
 }
