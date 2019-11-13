5dbebe441d9905f33a400b81d40193ca383df6e8
abdoul gbadamassi <gbadhanane@gmail.com>
2019-11-13 03:30:05 +0000

    commit

--- main.cpp
+++ main.cpp
@@ -12,5 +12,5 @@ int main() {
     //another method to print 
     cout<<"Here is another method to print"<< end;
     Ugrad *k1= new Ugrad(3.5, "CS", "123", "Paul");
-    k1->print
+    k1->print();
 }
